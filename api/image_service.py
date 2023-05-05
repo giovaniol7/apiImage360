25488e91b7b201011f265e292eea6bcef1bc7d6f
giovaniol7 <gihola@hotmail.com>
2023-05-05 20:27:58 -0300

    55

--- api/image_service.py
+++ api/image_service.py
@@ -2,47 +2,20 @@ import cv2
 from flask import Flask, Blueprint, request, jsonify, Response, send_file
 import numpy as np
 import requests
-import os
+import werkzeug
 
 image = Blueprint('image',__name__)
-
-image.config['IMAGES_FOLDER'] = 'caminho/para/a/pasta/de/imagens'
 
 @image.route('/')
 def instructions():
     return "Image"  
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in image.config['ALLOWED_EXTENSIONS']
-
 @image.route('/upload', methods=['POST'])
 def upload():
-    # montar o caminho completo para o arquivo de imagem
-    filepath = os.path.join(image.config['IMAGES_FOLDER'], filename)
-    # verificar se o arquivo de imagem existe
-    if not os.path.isfile(filepath):
-        return Response(status=404)
-    # retornar a imagem como uma resposta com o tipo de mídia correto
-    return send_file(filepath, mimetype='image/jpeg')
-
-def stitch_images(files):
-    stitcher = cv2.createStitcher()
-    images = [cv2.imread(file) for file in files]
-    (status, result) = stitcher.stitch(images)
-    if status == cv2.Stitcher_OK:
-        return result
-    else:
-        return 'Erro ao unir imagens: código de status {}'.format(status)
-
-@image.route('/stitch/<imgList>', methods=['GET'])
-def stitch():
-    files = request.get_json()['imgList']
-    result = stitch_files(files)
-    if isinstance(result, str):
-        return result
-    else:
-        # converte a imagem para o formato JPEG
-        _, img_encoded = cv2.imencode('.jpg', result)
-        response = Response(response=img_encoded.tobytes(), content_type='image/jpeg')
-        return response
+    if(request.methods == "POST"):
+        imagefile = request.file['imageFile']
+        filename = werkzeug.utils.secure_filename(imagefile.filename)
+        imagefile.save = ("./output/" + filename)
+        return jsonify({
+            "message": "Imagem uploud com sucesso"
+        })
