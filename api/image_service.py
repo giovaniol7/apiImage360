60688e17182c4a3fd8729f53f9ce339cbc3f2e72
giovaniol7 <gihola@hotmail.com>
2023-05-08 15:13:17 -0300

    new

--- api/image_service.py
+++ api/image_service.py
@@ -21,7 +21,7 @@ def upload():
 
         if os.path.exists(pasta):
             for arquivo in os.listdir(pasta):
-                if arquivo.endswith('.png'):
+                if arquivo.endswith('.jpeg'):
                     os.remove(os.path.join(pasta, arquivo))
 
         pasta2 = './upload/'
@@ -42,7 +42,7 @@ def get_stitched_image():
     # Call stitch_images() to stitch the images and save the stitched image
     stitch_images()
 
-    stitched_image_path = 'stitchImage/stitched_image.png'
+    stitched_image_path = 'stitchImage/stitched_image.jpeg'
 
     pasta = './upload/'
     pasta2 = './stitchImage/'
@@ -51,17 +51,15 @@ def get_stitched_image():
         return jsonify({'error': 'Stitched image not found.'}), 404
 
     for arquivo in os.listdir(pasta):
-        if arquivo.endswith('.png'):
-            os.remove(os.path.join(pasta, arquivo))
-        if arquivo.endswith('.jpg'):
+        if arquivo.endswith('.jpeg'):
             os.remove(os.path.join(pasta, arquivo))
 
-    return send_file(stitched_image_path, mimetype='image/png')
+    return send_file(stitched_image_path, mimetype='image/jpeg')
 
 
 def stitch_images():
     upload_dir = 'upload'
-    image_paths = [os.path.join(upload_dir, f) for f in os.listdir(upload_dir) if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.jpeg')]
+    image_paths = [os.path.join(upload_dir, f) for f in os.listdir(upload_dir) if f.endswith('.jpeg')]
 
     images = []
     for path in image_paths:
@@ -104,7 +102,7 @@ def stitch_images():
         output_dir = 'stitchImage'
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-        output_path = os.path.join(output_dir, 'stitched_image.png')
+        output_path = os.path.join(output_dir, 'stitched_image.jpeg')
         cv2.imwrite(output_path, stitched_image)
 
         return jsonify({'message': 'Image success.'}), 200
